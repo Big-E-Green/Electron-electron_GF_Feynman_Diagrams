ae64f71a1a39c9b4bc2d444d69cc2871ab912ccd
Big-E-Green <105460863+Big-E-Green@users.noreply.github.com>
2022-05-26 01:23:34 +0100

    Connected ONLY [WORKING]
    through true false statements, proves if any n is connected in any way to the a-b line if no, is disconnected and removed. Working for n serise diagrams.

--- disconnecteddiagrams.py
+++ disconnecteddiagrams.py
@@ -36,81 +36,73 @@ def reachzero(start,array):
                 hold.append(k)
                 RAY.remove(k)
                 if k[1]=='00':
-                    RAY=[]
                     return True, hold
             if c>10:
-                i=[]
                 return False, hold
             
 def loopcheck(var,i):
     c=0
     hold=[]
+    tmp=var
     while len(i)!=0:
-        c=c+1
-        tmp=var
+        c=c+1      
         for k in i:
             if k[0]==tmp:
-                tmp=k[1]
-                if k[0]==k[1]:
-                    return True, print('small loop')
-                if k[1]==var:
-                    return True, print('big loop')
+                if k[1]!='00':
+                    tmp=k[1]
+                    hold.append(k)
+                    if k[0]==k[1]:
+                        return True, hold
+                    if k[1]==var:
+                        return True, hold
             if c>10:
                 i=[]
                 return False, hold
-#for i in ARRAY:
- #   input()
-  #  print(i)
-   # print(reachzero('1p',i))
 
 
 
-for i in ARRAY:
-    input()
-    print(i)
+connected=[]
+for z in ARRAY:
+    sigma=cheeko.copy()
+    i=z.copy()
     for x in cheeko:
+        arr=[]
+        for j in x:          
+            zero=reachzero(j,i)
+            arr.append(zero[0])
+            if len(arr)==2:
+                if True in arr:
+                    sigma.remove(x)
+    for x in cheeko:
+        arr2=[]
+        arr3=[]
         for j in x:
-            print(j)
-            #print(reachzero(j,i),'zero')           
-            print(loopcheck(j,i))
+            looper=loopcheck(j,i)
+            arr2.append(looper[0])
+            arr3.append(looper[1])
+            if len(arr2)==2:
+                if arr2[0] and arr2[1]==True:
+                    for i in arr3:
+                        for j in i:
+                            for k in j:
+                                for q in sigma:
+                                    if k not in q:
+                                        sigma.remove(x)
+                            
+    if len(sigma)==0:
+        connected.append(z)
 
+print(len(connected))       
+print(connected)
 
                 
-            
-            
 
-c=-1
-DISCONNN=[]
-disconnn2=[]
-remm=disconnnn.copy()
-for i in disconnnn:
-    if len(i)<5:
-        DISCONNN.append(i)
-for i in DISCONNN:
-    remm.remove(i)
-    disconnn2.append(i)
-FULL=full.copy()
-for i in remm:
-    FULL.remove(i)
-    disconnn2.append(i)
 
-DISSC=[]   
-for i in FULL:
-    if ['1p', '1p'] in i:
-        if ['11', '11'] in i:
-            DISSC.append(i)
-    if ['2p', '2p'] in i:
-        if ['22', '22'] in i:
-            DISSC.append(i)   
-    if ['3p', '3p'] in i:
-        if ['33', '33'] in i:
-            DISSC.append(i)
 
-discoonnn3=[]
-for i in DISSC:
-    if i in FULL:
-        FULL.remove(i)
-        discoonnn3.append(i)
 
-print(FULL)
-print(len(FULL))                    #ONLY CONNECTED DIAGRAMS LEFT
+
+
+
+
+
+
