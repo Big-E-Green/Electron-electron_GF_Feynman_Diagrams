973d7a5487fd62ba26d1d560fed4109047228aec
Big-E-Green <105460863+Big-E-Green@users.noreply.github.com>
2022-06-08 02:08:11 +0100

    made changes

--- topologicalEquivilence.py
+++ topologicalEquivilence.py
@@ -1,9 +1,41 @@
-from RemovingDisconnected import *
-from generator import *
+from dd import *
+from generator2 import *
+from functions import *
 import itertools
 from copy import deepcopy
+import timeit
+
+
+def swap(translation,numm):
+    cc=[]
+    for i in translation: 
+        c=0
+        for j in numm:
+            c=c+1
+            if j==i:
+                cc.append(c)
+                if len(cc)==2:
+                    return cc
+                    
+def reparam(trans,inp,numm):
+    c=-1
+    ii=deepcopy(inp)
+    for k in inp:
+        c=c+1
+        c2=-1
+        for q in k:
+            c2=c2+1
+            if q==trans[0]:        
+                ii[c][c2]=trans[1]
+            if q==trans[1]:
+                ii[c][c2]=trans[0]
+    sw=swap(trans,numm)
+    ii[sw[0]],ii[sw[1]]=ii[sw[1]],ii[sw[0]]
+    return ii
+
 
 def distinctDiagrams(n):
+    s=timeit.default_timer()
     gen=genny(n)
     nums=[]
     for i in gen:
@@ -14,37 +46,10 @@ def distinctDiagrams(n):
     cool=[]
     for i in lev:
         c=c+1
-        if c>1:
+        if c<2:
             dunce=list(itertools.combinations(lev,c))
             cool.append(dunce)
-
-    def swap(translation):
-        cc=[]
-        for i in translation: 
-            c=0
-            for j in nums:
-                c=c+1
-                if j==i:
-                    cc.append(c)
-                    if len(cc)==2:
-                        return cc
-                    
-    def reparam(trans,inp):
-        c=-1
-        ii=deepcopy(inp)
-        for k in inp:
-            c=c+1
-            c2=-1
-            for q in k:
-                c2=c2+1
-                if q==trans[0]:        
-                    ii[c][c2]=trans[1]
-                if q==trans[1]:
-                    ii[c][c2]=trans[0]
-        sw=swap(trans)
-        ii[sw[0]],ii[sw[1]]=ii[sw[1]],ii[sw[0]]
-        return ii
-
+    print(cool)
     conn=connectedDiagrams(n)
     connn=deepcopy(conn)
     for i in connn:
@@ -54,8 +59,8 @@ def distinctDiagrams(n):
                 c=0
                 while c!=len(k):
                     for q in k:
-                        gens=reparam(q,tmp)
-                        tmp=gens                                    
+                        gens=reparam(q,tmp,nums)
+                        tmp=gens                                  
                         c=c+1
                         if c==len(k):
                             if gens in connn:
@@ -64,19 +69,12 @@ def distinctDiagrams(n):
     con2=deepcopy(connn)
     for i in con2:
         for j in lev:
-            gens=reparam(j,i)
+            gens=reparam(j,i,nums)
             if gens in con2:                                        
                 if gens!=i:
                     con2.remove(gens)
+    st=timeit.default_timer()
+    print('Time Inequiv:',st-s)
     return con2
-
-
-
-
-
-
-
-
-
-
-
+#print(distinctDiagrams(4))
+print(len(distinctDiagrams(2)))
